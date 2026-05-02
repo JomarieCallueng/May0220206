0499424b139394f030dcf55ee7f695d745269e6f
COLLEGELAB-23 <COLLEGELAB-23@BJSB.LAB>
2026-05-02 17:33:01 +0800

    added card property and used array
    a

--- May022026-app/src/App.jsx
+++ May022026-app/src/App.jsx
@@ -2,12 +2,29 @@
 import './App.css'
 import Card from './Card'
 function App() {
+  const carditems = [
+    {header:"Chapter 1", title:"Objective", description:"Objective of the study", buttontext:"View Scope and Delimitation"},
+    {header:"Chapter 2", title:"Review of Related Literature", description:"Related System", buttontext:"Show More"},
+    {header:"Chapter 3", title:"Methodology", description:"Method of Development", buttontext:"See Charts"}
+  ]
+
   return(
     <>
     <h1>Hello</h1>
-    <Card>
-      
-    </Card>
+   
+     {carditems.map(item=>(
+      <Card header={item.header}
+        title={item.title}
+        description={item.description}
+        buttontext={item.buttontext}
+>
+       
+    
+      </Card>
+    )
+  )
+}
+
     </>
   )
 }
